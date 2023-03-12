d51817986b49beccdcfdf4ff22756b389c09c381
Tanuj <t.shirkhedkar@gmail.com>
2023-03-11 22:07:20 -0500

    reverses given string

--- src/com/apcs/ReverseString.java
+++ src/com/apcs/ReverseString.java
@@ -1,4 +1,23 @@
 package com.apcs;
 
 public class ReverseString {
+    private String oldString;
+
+    public ReverseString(String str){
+        oldString = str;
+    }
+    public String reverse(){
+        String returnString = "";
+        for(int i = oldString.length()-1; i >= 0; i--){
+            returnString += oldString.charAt(i);
+
+        }
+        return returnString;
+    }
+    public static void main(String[] args){
+        ReverseString str = new ReverseString("enola");
+        System.out.println(str.reverse());
+
+    }
+
 }
